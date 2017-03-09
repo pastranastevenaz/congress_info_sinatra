490c82e78e8738603959dd75d769ea96dfaf1223
Steven Antonio <pastrana.steven.az@gmail.com>
2017-03-08 20:09:13 -0700

    download link works

--- web.rb
+++ web.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 
-set :public_folder, '/app/house_bills/public'
-
+set :public_folder, File.dirname(__FILE__) 
+set :root, File.dirname(__FILE__)
+# set :public_folder, File.dirname(__FILE__) + "/app/house_bills/public"
 get '/' do
   @title = "Current Bills"
   erb :index
