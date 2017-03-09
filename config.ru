af3030a630e2f9b0f7415187c86ef6bd96cf699c
Steven Antonio <pastrana.steven.az@gmail.com>
2017-03-08 20:25:33 -0700

    added now root file to fix web.rb not adding to git

--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require './web'
+require './startApp'
 run Sinatra::Application
---
+++ startApp.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+set :public_folder, File.dirname(__FILE__)
+set :root, File.dirname(__FILE__)
+# set :public_folder, File.dirname(__FILE__) + "/app/house_bills/public"
+get '/' do
+  @title = "Current Bills"
+  erb :index
+end
+
+get '/currrent_house/' do
+  "Hello"
+end
